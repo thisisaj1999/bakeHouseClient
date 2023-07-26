f2a2cbc50204a8de211c2ab0758b9381400f0647
thisisaj1999 <atuljoshi94170@gmail.com>
2023-07-26 23:25:22 +0530

    Fixed the url for login

--- src/services/services.js
+++ src/services/services.js
@@ -4,7 +4,7 @@ const baseUrl = process.env.REACT_APP_SERVER_URL
 
 export const loginUser = async (data) => {
     try {
-        const response = await axios.post(`${baseUrl}/auth/login`, data);
+        const response = await axios.post(`${baseUrl}/api/users/login`, data);
         return response;
     } catch (error) {
         console.log(error);
