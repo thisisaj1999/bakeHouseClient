006f61dc527bb8b68b06591ebe0b7a4cd04d9c82
thisisaj1999 <atuljoshi94170@gmail.com>
2023-07-28 23:58:34 +0530

    Used toast to notify the token status

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState, useMemo } from "react";
 import SideNavbar from "../../components/SideNavbar/SideNavbar";
 import { useNavigate } from "react-router-dom";
+import { useSnackbar } from 'notistack'
 
 const Dashboard = () => {
 	const [user, setUser] = useState();
+  const { enqueueSnackbar } = useSnackbar()
 
   const navigate = useNavigate();
 
 	useEffect(() => {
 		if (localStorage.getItem("token") === null) {
+      enqueueSnackbar('Un-verified User', {variant : 'error'});
 			navigate("/login");
 		}
 		setUser(JSON.parse(localStorage.getItem("userData")));
