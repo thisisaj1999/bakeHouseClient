bc195fddae0226afafe0e56c68c115c3d09698a7
thisisaj1999 <atuljoshi94170@gmail.com>
2023-07-29 23:22:03 +0530

    Created items card

---
+++ src/layouts/Items/ItemCards.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function ItemCards({sideBar}) {
+    return (
+      <Card sx={{ maxWidth: sideBar ? 210 : 240 }}>
+        <CardMedia
+          sx={{ height: sideBar ? 130 : 150 }}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    );
+  }
